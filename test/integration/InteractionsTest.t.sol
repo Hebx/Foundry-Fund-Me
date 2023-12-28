ec9b3767bbfe0304dc8d725f5ec54e686cde1712
Hebx <lord.noctx@gmail.com>
2023-12-29 00:56:46 +0100

    Added Integration Test for Interactions with the smart contract

---
+++ test/integration/InteractionsTest.t.sol
@@ -0,0 +1,35 @@
+//SPDX-LICENSE-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../../src/FundMe.sol";
+import {DeployFundMe} from "../../script/DeployFundMe.s.sol";
+import {FundFundMe, WithdrawFundMe} from "../../script/interactions.s.sol";
+
+contract InteractionsTest is Test {
+    FundMe fundMe;
+    uint256 constant SEND_VALUE = 0.1 ether; // 1e17
+    address USER = makeAddr("user");
+    uint256 STARTING_BALANCE = 10 ether;
+    uint256 GAS_PRICE = 1;
+
+    function setUp() external {
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+        vm.deal(USER, STARTING_BALANCE);
+    }
+
+    function testUserCanFundInteractions() public {
+        FundFundMe fundFundMe = new FundFundMe();
+        // vm.prank(USER);
+        // vm.deal(USER, 1e18);
+        fundFundMe.fundFundMe(address(fundMe));
+
+        // address funder = fundMe.getFunder(0);
+        // assertEq(funder, USER);
+        WithdrawFundMe withdrawFundMe = new WithdrawFundMe();
+        withdrawFundMe.withdrawFundMe(address(fundMe));
+        assert(address(fundMe).balance == 0);
+    }
+}
